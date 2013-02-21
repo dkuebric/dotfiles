fc3babff3de48404239c5ccaa4824883c39a0a6b
Dan Kuebrich <dan.kuebrich@gmail.com>
2013-02-21 19:40:38 +0000

    [readme] add initialization script and instructions to a future dan

---
+++ ravage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CWD=`pwd`
+
+# bash
+rm ~/.bashrc
+rm ~/.bash_aliases
+rm ~/.bash_logout
+rm ~/.inputrc
+
+# vim
+rm ~/.vimrc
+rm ~/.vim
+
+# git
+rm ~/.gitconfig
+
+# osx
+rm ~/.profile
